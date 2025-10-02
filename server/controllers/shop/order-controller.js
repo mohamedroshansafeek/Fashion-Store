2a67a9d477b55d48e20bd032687ca8b6a631988d
mohamedroshansafeek <mohamedroshansafeeky@gmail.com>
2025-10-02 15:39:33 +0530

    Updated URL

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -39,8 +39,8 @@ const createOrder = async (req, res) => {
         },
       ],
       application_context: {
-        return_url: "https://fashion-store-pi-wine.vercel.app/shop/paypal-return",
-        cancel_url: "https://fashion-store-pi-wine.vercel.app/shop/paypal-cancel",
+        return_url: "https://fashion-store-two-cyan.vercel.app/shop/paypal-return",
+        cancel_url: "https://fashion-store-two-cyan.vercel.app/shop/paypal-cancel",
       },
     });
 
