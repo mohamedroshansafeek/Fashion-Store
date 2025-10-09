788eda7f30cbf1c0c6e0828c4e138d3abac4f757
mohamedroshansafeek <mohamedroshansafeeky@gmail.com>
2025-10-09 23:13:30 +0530

    Updated

--- client/src/components/admin-view/orders.jsx
+++ client/src/components/admin-view/orders.jsx
@@ -1,117 +1,7 @@
-// import { useEffect, useState } from "react";
-// import { Button } from "../ui/button";
-// import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-// import { Dialog } from "../ui/dialog";
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "../ui/table";
-// import AdminOrderDetailsView from "./order-details";
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//   getAllOrdersForAdmin,
-//   getOrderDetailsForAdmin,
-//   resetOrderDetails,
-// } from "@/store/admin/order-slice";
-// import { Badge } from "../ui/badge";
-
-// function AdminOrdersView() {
-//   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
-//   const { orderList, orderDetails } = useSelector((state) => state.adminOrder);
-//   const dispatch = useDispatch();
-
-//   function handleFetchOrderDetails(getId) {
-//     dispatch(getOrderDetailsForAdmin(getId));
-//   }
-
-//   useEffect(() => {
-//     dispatch(getAllOrdersForAdmin());
-//   }, [dispatch]);
-
-//   console.log(orderDetails, "orderList");
-
-//   useEffect(() => {
-//     if (orderDetails !== null) setOpenDetailsDialog(true);
-//   }, [orderDetails]);
-
-//   return (
-//     <Card>
-//       <CardHeader>
-//         <CardTitle>All Orders</CardTitle>
-//       </CardHeader>
-//       <CardContent>
-//         <Table>
-//           <TableHeader>
-//             <TableRow>
-//               <TableHead>Order ID</TableHead>
-//               <TableHead>Order Date</TableHead>
-//               <TableHead>Order Status</TableHead>
-//               <TableHead>Order Price</TableHead>
-//               <TableHead>
-//                 <span className="sr-only">Details</span>
-//               </TableHead>
-//             </TableRow>
-//           </TableHeader>
-//           <TableBody>
-//             {orderList && orderList.length > 0
-//               ? orderList.map((orderItem) => (
-//                   <TableRow>
-//                     <TableCell>{orderItem?._id}</TableCell>
-//                     <TableCell>{orderItem?.orderDate.split("T")[0]}</TableCell>
-//                     <TableCell>
-//                       <Badge
-//                         className={`py-1 px-3 ${
-//                           orderItem?.orderStatus === "confirmed"
-//                             ? "bg-green-500"
-//                             : orderItem?.orderStatus === "rejected"
-//                             ? "bg-red-600"
-//                             : "bg-black"
-//                         }`}
-//                       >
-//                         {orderItem?.orderStatus}
-//                       </Badge>
-//                     </TableCell>
-//                     <TableCell>${orderItem?.totalAmount}</TableCell>
-//                     <TableCell>
-//                       <Dialog
-//                         open={openDetailsDialog}
-//                         onOpenChange={() => {
-//                           setOpenDetailsDialog(false);
-//                           dispatch(resetOrderDetails());
-//                         }}
-//                       >
-//                       <div className="flex flex-col gap-2">
-//                         <Button
-//                           onClick={() =>
-//                             handleFetchOrderDetails(orderItem?._id)
-//                           }
-//                         >
-//                           View Details
-//                         </Button>
-//                         <AdminOrderDetailsView orderDetails={orderDetails} />
-//                       </div>
-//                       </Dialog>
-//                     </TableCell>
-//                   </TableRow>
-//                 ))
-//               : null}
-//           </TableBody>
-//         </Table>
-//       </CardContent>
-//     </Card>
-//   );
-// }
-
-// export default AdminOrdersView;
-
 import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { Dialog, DialogTrigger, DialogContent } from "../ui/dialog";
+import { Dialog } from "../ui/dialog";
 import {
   Table,
   TableBody,
@@ -142,33 +32,34 @@ function AdminOrdersView() {
     dispatch(getAllOrdersForAdmin());
   }, [dispatch]);
 
+  console.log(orderDetails, "orderList");
+
   useEffect(() => {
     if (orderDetails !== null) setOpenDetailsDialog(true);
   }, [orderDetails]);
 
   return (
-    <Card className="w-full">
+    <Card>
       <CardHeader>
         <CardTitle>All Orders</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="overflow-x-auto w-full">
-          <Table className="min-w-[600px]">
-            <TableHeader>
-              <TableRow>
-                <TableHead>Order ID</TableHead>
-                <TableHead>Order Date</TableHead>
-                <TableHead>Order Status</TableHead>
-                <TableHead>Order Price</TableHead>
-                <TableHead>
-                  <span className="sr-only">Details</span>
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {orderList && orderList.length > 0 ? (
-                orderList.map((orderItem) => (
-                  <TableRow key={orderItem._id}>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+              <TableHead>
+                <span className="sr-only">Details</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orderList && orderList.length > 0
+              ? orderList.map((orderItem) => (
+                  <TableRow>
                     <TableCell>{orderItem?._id}</TableCell>
                     <TableCell>{orderItem?.orderDate.split("T")[0]}</TableCell>
                     <TableCell>
@@ -193,36 +84,26 @@ function AdminOrdersView() {
                           dispatch(resetOrderDetails());
                         }}
                       >
-                        <DialogTrigger asChild>
-                          <Button
-                            size="sm"
-                            onClick={() =>
-                              handleFetchOrderDetails(orderItem?._id)
-                            }
-                          >
-                            View Details
-                          </Button>
-                        </DialogTrigger>
-                        <DialogContent>
-                          <AdminOrderDetailsView orderDetails={orderDetails} />
-                        </DialogContent>
+                        <Button
+                          onClick={() =>
+                            handleFetchOrderDetails(orderItem?._id)
+                          }
+                        >
+                          View Details
+                        </Button>
+                        <AdminOrderDetailsView orderDetails={orderDetails} />
                       </Dialog>
                     </TableCell>
                   </TableRow>
                 ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={5} className="text-center py-4">
-                    No orders found
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
+              : null}
+          </TableBody>
+        </Table>
       </CardContent>
     </Card>
   );
 }
 
 export default AdminOrdersView;
+
+
